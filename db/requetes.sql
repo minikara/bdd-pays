8a635c6470b1e2f13711620917ad589acbe15e42
minikara <diane.leblanc.albarel@gmail.com>
2019-05-13 16:52:52 +0200

    requetes

--- db/requetes.sql
+++ db/requetes.sql
@@ -1,3 +1,5 @@
-SELECT SUM(montant) AS montant_total FROM Impot WHERE idResident=<idX>;
-SELECT COUNT(id) FROM Resident INNER JOIN (AideSociale ON Resident.id=AideSociale.idResident) WHERE nationalite!=France;
-SELECT SUM(montant)FROM AideSociale CROSS JOIN (Resident ON Resident.id=AideSociale.idResident) WHERE nationalite!=France;
+SELECT SUM(montant) AS montant_total FROM Impot WHERE idResident="<idX>";
+SELECT COUNT(id) FROM Resident INNER JOIN (AideSociale ON Resident.id=AideSociale.idResident) WHERE nationalite!="France";
+SELECT SUM(montant)FROM AideSociale CROSS JOIN (Resident ON Resident.id=AideSociale.idResident) WHERE nationalite!="France";
+SELECT * FROM Impot WHERE etat="EnCoursDeValidation";
+SELECT * FROM AideSociale WHERE etat="EnCoursDeValidation";
